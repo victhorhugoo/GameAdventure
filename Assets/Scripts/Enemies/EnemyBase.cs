fa461a584a89e23db4d53b7f0ec6c21c38bff1dd
victhorhugoo <163677031+victhorhugoo@users.noreply.github.com>
2026-07-20 12:26:09 -0300

    feat: add enemies

---
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyBase : MonoBehaviour
+    {
+        public float startLife = 10f;
+
+        [SerializeField] private float _currentLife;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
+
+        private void Awake()
+        {
+            Init();
+            
+        }
+
+        protected void ResetLife()
+        {
+            _currentLife = startLife;
+        }
+
+        protected virtual void Init()
+        {
+            ResetLife();
+            if(startWithBornAnimation)
+                BornAnimation();
+        }
+
+        protected virtual void Kill()
+        {
+            OnKill();
+        }
+
+        protected virtual void OnKill()
+        {
+            Destroy(gameObject);
+        }
+
+        public void OnDamage(float f)
+        {
+            _currentLife -= f;
+
+            if(_currentLife <= 0)
+            {
+                Kill();
+            }
+        }
+
+        #region ANIMATION
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+
+        #endregion
+
+        // Debug
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                OnDamage(5f);
+            }
+        }
+    }
+}
